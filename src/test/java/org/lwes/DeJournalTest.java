3961c5991ef84f883f88b0b0f4eb932b77733c04
Frank Maritato <frankmarit@gmail.com>
2009-04-22 18:45:11 +0000

    Moved mock object to a separate class file. Added tests for the GZIPEventHandler
    git-svn-id: https://lwes.svn.sourceforge.net/svnroot/lwes/lwes-journaller-java/trunk@106 a2f82657-cdd2-4550-bd36-68a8e7111808

--- src/test/java/org/lwes/DeJournalTest.java
+++ src/test/java/org/lwes/DeJournalTest.java
@@ -4,12 +4,11 @@ package org.lwes;
  * Date: Apr 21, 2009
  */
 
+import junit.framework.TestCase;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import junit.framework.TestCase;
 
 import java.util.List;
-import java.util.LinkedList;
 
 public class DeJournalTest extends TestCase {
     private transient Log log = LogFactory.getLog(DeJournalTest.class);
@@ -27,23 +26,6 @@ public class DeJournalTest extends TestCase {
         catch (Exception e) {
             log.error(e.getMessage(), e);
             fail(e.getMessage());
-        }
-    }
-
-    class MockDeJournaller extends DeJournaller {
-        private List<Event> eventList = new LinkedList<Event>();
-
-        @Override
-        public void handleEvent(Event event) {
-            eventList.add(event);
-        }
-
-        public List<Event> getEventList() {
-            return eventList;
-        }
-
-        public void setEventList(List<Event> eventList) {
-            this.eventList = eventList;
         }
     }
 }
---
+++ src/test/java/org/lwes/MockDeJournaller.java
@@ -0,0 +1,30 @@
+package org.lwes;
+/**
+ * User: fmaritato
+ * Date: Apr 22, 2009
+ */
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class MockDeJournaller extends DeJournaller {
+    private transient Log log = LogFactory.getLog(MockDeJournaller.class);
+
+    private List<Event> eventList = new LinkedList<Event>();
+
+    @Override
+    public void handleEvent(Event event) {
+        eventList.add(event);
+    }
+
+    public List<Event> getEventList() {
+        return eventList;
+    }
+
+    public void setEventList(List<Event> eventList) {
+        this.eventList = eventList;
+    }
+}
