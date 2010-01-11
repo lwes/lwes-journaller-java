c109f92c582fa351da9283d1ea059da950096532
Frank Maritato <frankmarit@gmail.com>
2010-01-11 19:07:48 +0000

    making changes for 0.0.7 release: journaller emits health and rotate events for monitoring purposes.
    git-svn-id: https://lwes.svn.sourceforge.net/svnroot/lwes/lwes-journaller-java/trunk@388 a2f82657-cdd2-4550-bd36-68a8e7111808

---
+++ src/main/java/org/lwes/journaller/event/Health.java
@@ -0,0 +1,75 @@
+package org.lwes.journaller.event;
+/**
+ * @author fmaritato
+ */
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.lwes.Event;
+import org.lwes.EventSystemException;
+import org.lwes.NoSuchAttributeException;
+
+import java.math.BigInteger;
+
+public class Health extends Event {
+
+    private static transient Log log = LogFactory.getLog(Health.class);
+
+    // Constants used as attribute keys
+    public static final String NAME = "Journaller::Health";
+
+    public static final String TS = "ts";
+    public static final String NUM = "num";
+    public static final String INTERVAL = "interval";
+
+    public Health() throws EventSystemException {
+        super(NAME, null);
+    }
+
+    public Health(long timestamp, long numEvents, int interval)
+            throws EventSystemException {
+        super(NAME, null);
+        setTimestamp(timestamp);
+        setNum(numEvents);
+        setInterval(interval);
+    }
+
+
+    public void setTimestamp(long millis) throws EventSystemException {
+        setUInt64(TS, millis);
+    }
+
+    public BigInteger getTimestamp() throws NoSuchAttributeException {
+        return getUInt64(TS);
+    }
+
+    public void setNum(long numEvents) throws EventSystemException {
+        setUInt64(NUM, numEvents);
+    }
+
+    public BigInteger getNum() throws NoSuchAttributeException {
+        return getUInt64(NUM);
+    }
+
+    public void setInterval(int seconds) throws EventSystemException {
+        setInt32(INTERVAL, seconds);
+    }
+
+    public int getInterval() throws NoSuchAttributeException {
+        return getInt32(INTERVAL);
+    }
+
+    public String toString() {
+        StringBuilder buf = new StringBuilder(256);
+        try {
+            buf.append("Health {ts=").append(getTimestamp())
+                    .append("; num=").append(getNum())
+                    .append("; interval=").append(getInterval())
+                    .append("}");
+        }
+        catch (NoSuchAttributeException e) {
+            log.error(e.getMessage(), e);
+        }
+        return buf.toString();
+    }
+}
---
+++ src/main/java/org/lwes/journaller/event/Rotate.java
@@ -0,0 +1,70 @@
+package org.lwes.journaller.event;
+/**
+ * @author fmaritato
+ */
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.lwes.Event;
+import org.lwes.EventSystemException;
+import org.lwes.NoSuchAttributeException;
+
+import java.math.BigInteger;
+
+public class Rotate extends Event {
+    private static transient Log log = LogFactory.getLog(Rotate.class);
+
+    // Constants used as attribute keys
+    public static final String NAME = "Journaller::Rotate";
+
+    public static final String TS = "ts";
+    public static final String NUM = "num";
+    public static final String FILENAME = "filename";
+
+    public Rotate() throws EventSystemException {
+        super(NAME, null);
+    }
+
+    public Rotate(long timestamp, long numEvents, String filename)
+            throws EventSystemException {
+        super(NAME, null);
+        setTimestamp(timestamp);
+        setNum(numEvents);
+        setFilename(filename);
+    }
+
+    public void setTimestamp(long millis) throws EventSystemException {
+        setUInt64(TS, millis);
+    }
+    public BigInteger getTimestamp() throws NoSuchAttributeException {
+        return getUInt64(TS);
+    }
+
+    public void setNum(long numEvents) throws EventSystemException {
+        setUInt64(NUM, numEvents);
+    }
+    public BigInteger getNum() throws NoSuchAttributeException {
+        return getUInt64(NUM);
+    }
+
+    public void setFilename(String filename) throws EventSystemException {
+        setString(FILENAME, filename);
+    }
+    public String getFilename() throws NoSuchAttributeException {
+        return getString(FILENAME);
+    }
+
+    public String toString() {
+        StringBuilder buf = new StringBuilder(256);
+        try {
+            buf.append("Rotate {ts=").append(getTimestamp())
+                    .append("; num=").append(getNum())
+                    .append("; filename=").append(getFilename())
+                    .append("}");
+        }
+        catch (NoSuchAttributeException e) {
+            log.error(e.getMessage(), e);
+        }
+        return buf.toString();
+    }
+}
