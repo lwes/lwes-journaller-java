c7e3df624f1d924c67e3f84e069d200e9d9f3190
Frank Maritato <frankmarit@gmail.com>
2010-08-28 00:00:40 +0000

    initial checkin
    git-svn-id: https://lwes.svn.sourceforge.net/svnroot/lwes/lwes-journaller-java/trunk@543 a2f82657-cdd2-4550-bd36-68a8e7111808

---
+++ src/test/java/org/lwes/journaller/MockSequenceDeJournaller.java
@@ -0,0 +1,31 @@
+package org.lwes.journaller;
+/**
+ * User: fmaritato
+ * Date: Apr 22, 2009
+ */
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.lwes.Event;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class MockSequenceDeJournaller extends SequenceDejournaller {
+    private transient Log log = LogFactory.getLog(MockSequenceDeJournaller.class);
+
+    private List<Event> eventList = new LinkedList<Event>();
+
+    @Override
+    public void handleEvent(Event event) {
+        eventList.add(event);
+    }
+
+    public List<Event> getEventList() {
+        return eventList;
+    }
+
+    public void setEventList(List<Event> eventList) {
+        this.eventList = eventList;
+    }
+}
