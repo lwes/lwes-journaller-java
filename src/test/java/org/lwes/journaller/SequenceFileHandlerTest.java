357d7be0d63cfc45eb4294d7abe509b7fa3cd9e4
Frank Maritato <frankmarit@gmail.com>
2010-08-28 00:00:20 +0000

    beefed up the test
    git-svn-id: https://lwes.svn.sourceforge.net/svnroot/lwes/lwes-journaller-java/trunk@542 a2f82657-cdd2-4550-bd36-68a8e7111808

--- src/test/java/org/lwes/journaller/SequenceFileHandlerTest.java
+++ src/test/java/org/lwes/journaller/SequenceFileHandlerTest.java
@@ -16,26 +16,37 @@ import org.lwes.listener.DatagramQueueElement;
 import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.List;
 
 public class SequenceFileHandlerTest extends TestCase {
 
     private transient Log log = LogFactory.getLog(SequenceFileHandlerTest.class);
-     
-    /**
-     * Manually inject 10 events into the GZIP event handler, force a rotation,
-     * verify the number of events in the first file, throw 10 more events, shutdown
-     * the handler then verify the events in the rotated file.
-     */
+
     public void testHandler() {
         try {
-            //SequenceFileHandler handler = new SequenceFileHandler("target/junit-seq-%tY%tm%td%tH%tM%tS");
             SequenceFileHandler handler = new SequenceFileHandler("target/junit-seq");
             String generatedFile1 = handler.getFilename();
             if (log.isDebugEnabled()) {
                 log.debug("generated file: " + generatedFile1);
             }
             handler.handleEvent(createTestEvent());
+            handler.handleEvent(createTestEvent());
             handler.destroy();
+
+            MockSequenceDeJournaller mdj = new MockSequenceDeJournaller();
+            mdj.setFileName(generatedFile1);
+            mdj.run();
+
+            List<Event> eventList = mdj.getEventList();
+            assertNotNull("Event list was null", eventList);
+            assertEquals("Number of events is wrong", 2, eventList.size());
+            for (Event evt : eventList) {
+                InetAddress addr = InetAddress.getByAddress(evt.getIPAddress(JournallerConstants.SENDER_IP));
+                assertEquals("192.168.1.1", addr.getHostAddress());
+                assertEquals(9191, (int) evt.getUInt16(JournallerConstants.SENDER_PORT));
+                assertNotNull(evt.getInt64(JournallerConstants.RECEIPT_TIME));
+                assertEquals(256, (int) evt.getInt32("intField1"));
+            }
         }
         catch (Exception e) {
             log.error(e.getMessage(), e);
