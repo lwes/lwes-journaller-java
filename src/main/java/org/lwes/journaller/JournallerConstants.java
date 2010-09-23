7d88a9ccabf54787203d0ece65d8ad15521b4384
Frank Maritato <frankmarit@gmail.com>
2010-09-23 04:10:32 +0000

    fixed MAX_BODY and MAX_MESSAGE
    git-svn-id: https://lwes.svn.sourceforge.net/svnroot/lwes/lwes-journaller-java/trunk@564 a2f82657-cdd2-4550-bd36-68a8e7111808

--- src/main/java/org/lwes/journaller/JournallerConstants.java
+++ src/main/java/org/lwes/journaller/JournallerConstants.java
@@ -6,8 +6,8 @@ package org.lwes.journaller;
 public interface JournallerConstants {
 
     public static final int MAX_HEADER_SIZE = 22;
-    public static final int MAX_BODY_SIZE = 65513;
-    public static final int MAX_MSG_SIZE = MAX_HEADER_SIZE + MAX_BODY_SIZE;
+    public static final int MAX_BODY_SIZE = 65507;
+    public static final int MAX_MSG_SIZE = 65535;
 
     static final String RECEIPT_TIME = "ReceiptTime";
     static final String SENDER_PORT = "SenderPort";
